61408447b40210985ad61dca3bebee8dda5a2ab8
juanpablo <103393257+Jitlastuden@users.noreply.github.com>
2022-04-10 20:41:58 -0400

    Add files via upload

---
+++ login/src/formularios/frmBienvenida.java
@@ -0,0 +1,50 @@
+package formularios;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class frmBienvenida extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frmBienvenida frame = new frmBienvenida();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public frmBienvenida() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHola = new JLabel("HOLA !");
+		lblHola.setBounds(116, 97, 46, 14);
+		contentPane.add(lblHola);
+	}
+
+}
